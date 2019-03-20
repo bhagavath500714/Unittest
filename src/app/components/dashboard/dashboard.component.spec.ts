c7209fe045eee3c6dd48916340e69b66e75da6d2
bhagavathavigo <38946625+bhagavathavigo@users.noreply.github.com>
2019-03-20 19:00:35 +0530

    negative senario

--- src/app/components/dashboard/dashboard.component.spec.ts
+++ src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { isNumber } from 'util';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -33,5 +34,28 @@ describe('DashboardComponent', () => {
   });
   it('operation value should be once among predefined enum operations', () => {
       expect (Object.values(DashboardComponent.Operation)).toContain(dashboardComponent.operation);
+  });
+  it('age value should be a number', () => {
+      expect(isNumber(dashboardComponent.age));
+  });
+
+  //Negative Scenario Test 1
+  let nejDashboardComponent = new DashboardComponent();
+  nejDashboardComponent = Object.assign({}, dashboardComponent);
+  delete nejDashboardComponent.age;
+  delete nejDashboardComponent.operation;
+  it('Missing data from operation field', () => {
+    expect(nejDashboardComponent.operation).toBeUndefined();
+  });
+  it('Missing data from age field', () => {
+    expect(nejDashboardComponent.age).toBeUndefined();
+  });
+
+  //Negative Scenario Test 2
+  let nejtDashboardComponent = new DashboardComponent();
+  nejtDashboardComponent = Object.assign({}, dashboardComponent);
+  nejtDashboardComponent.age = 12;
+  it('age value is not valid number', () =>{
+    expect(isNaN(nejtDashboardComponent.age));
   });
 });
